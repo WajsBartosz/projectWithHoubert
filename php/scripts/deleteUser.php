ef6a6092c10b28fcf9619f15bc81de2cd97bfe15
WajsBartosz <bartezzwajs@gmail.com>
2022-02-17 23:54:24 +0100

    Adding deleteUser.php script

---
+++ php/scripts/deleteUser.php
@@ -0,0 +1,11 @@
+<?php 
+    // not final version
+    include "../connect.php";
+    function deleteUser($login){
+        $sql="DELETE FROM `users` WHERE `login`=$login";
+        $connect->query($sql);
+    }
+
+
+
+?>
